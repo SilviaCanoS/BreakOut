7f586a9d47d3093e1e30b6517a43f5f8a5989d10
SilviaCanoS <99064925+SilviaCanoS@users.noreply.github.com>
2023-05-27 02:17:42 -0600

    Agregar al jugador y la pelota
    - Desarrollo del script del jugador
    - Desarrollo del script de la pelota
    - Uso del input manager para probar diferentes modos de entrada

--- BreakOut/Assets/Scripts/Pelota.cs
+++ BreakOut/Assets/Scripts/Pelota.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Pelota : MonoBehaviour
 {
+    public bool isGameStarted = false;
+    [SerializeField] public float velocidadPelota = 20;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 posInicial = GameObject.FindGameObjectWithTag("Jugador").transform.position; //le da la posicion del jugador
+        posInicial.y += 2;
+        this.transform.position = posInicial;
+        this.transform.SetParent(GameObject.FindGameObjectWithTag("Jugador").transform); //hace que se mueva con el jugador
     }
 
     // Update is called once per frame
+    //input manager
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetButton("Submit")) //cuando se use la tecla espacio
+        {
+            if(!isGameStarted)
+            {
+                isGameStarted = true; //el juego empezo
+                this.transform.SetParent(null); //quita al jugador como padre de la pelota
+                GetComponent<Rigidbody>().velocity = velocidadPelota * Vector3.up; //poner la velocidad y mandar hacia arriba
+            }
+        }
     }
 }
